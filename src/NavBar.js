86ddf7189c1ae478c5d0fce25407d6ebb868c2db
LydiaSorbo <lesorbo@gmail.com>
2024-02-29 14:48:37 -0800

    formatted nav-bar

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaw } from '@fortawesome/free-solid-svg-icons';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 function Navbar() {
   return (
     <nav>
-      <h1>Husky Study Scout</h1>
-      <p>Scouting study dens, for Huskies by Huskies &#128062;</p>
-      <Link to="/pawsibilities">
-        <p id="PawsText">Pawsibilities</p>
-        <img className="paw" src="img/huskyPaw.png" width="60" height="60" alt="Paw Icon" />
-      </Link>
-      <Link to="/rating">
-        <p id="RateText">Rate</p>
-        <img className="rate" src="img/ratingIcon.png" width="60" height="60" alt="Rating Icon" />
-      </Link>
+      <div>
+        <h1>Husky Study Scout</h1>
+        <p className="subtitle">Scouting study dens, for Huskies by Huskies &#128062;</p>
+      </div>
+      <div className='navIcons'>
+        <Link to="/pawsibilities">
+            <div className='pawButton'>
+            <FontAwesomeIcon icon={faPaw} className="paw" size="3x" />
+            <p id="PawsText">Pawsibilities</p>
+          </div>
+        </Link>
+        <Link to="/rating">
+            <div className='rateButton'>
+            <FontAwesomeIcon icon={faStar} className="star" size="3x"/>
+            <p id="RateText">Rate</p>
+            </div>
+        </Link>
+      </div>
     </nav>
   );
 }
