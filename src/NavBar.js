abb00a88377213285c28a0bf5d1874e7095cde30
LydiaSorbo <lesorbo@gmail.com>
2024-02-29 15:34:31 -0800

    added route function

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,6 +3,10 @@ import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaw } from '@fortawesome/free-solid-svg-icons';
 import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { faHome } from '@fortawesome/free-solid-svg-icons';
+import Pawsibilities from './Pawsibilities';
+import Rating from './Rating';
+import Home from './Home';
 
 function Navbar() {
   return (
@@ -12,13 +16,19 @@ function Navbar() {
         <p className="subtitle">Scouting study dens, for Huskies by Huskies &#128062;</p>
       </div>
       <div className='navIcons'>
-        <Link to="/pawsibilities">
+        <Link to="/home" element={<Home />}>
+            <div className='homeButton'>
+            <FontAwesomeIcon icon={faHome} className="home" size="3x" />
+            <p id="HomeText">Home</p>
+          </div>
+        </Link>
+        <Link to="/pawsibilities" element={<Pawsibilities />}>
             <div className='pawButton'>
             <FontAwesomeIcon icon={faPaw} className="paw" size="3x" />
             <p id="PawsText">Pawsibilities</p>
           </div>
         </Link>
-        <Link to="/rating">
+        <Link to="/rating" element={<Rating />}>
             <div className='rateButton'>
             <FontAwesomeIcon icon={faStar} className="star" size="3x"/>
             <p id="RateText">Rate</p>
