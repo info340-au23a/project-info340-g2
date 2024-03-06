51ee68c69ac79770d83f37e39773ba1806b35292
LydiaSorbo <lesorbo@gmail.com>
2024-03-05 17:35:10 -0800

    small changes

--- src/components/StudySpotCard.js
+++ src/components/StudySpotCard.js
@@ -11,7 +11,6 @@ function StudySpotCard({ spot, addToPawsibilities, markAsVisited, showButton }) 
         <img src={spot.image} alt={spot.name} />
       </div>
       <div className="description">
-      <img src={spot.image} alt={spot.name} />
         <h2>{spot.name}</h2>
         <p>{spot.description}</p>
         <h3>Wifi Rating</h3>
