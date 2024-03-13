b9b2c202c21b0a813d494a8188c82b079231edc1
LydiaSorbo <lesorbo@gmail.com>
2024-03-12 20:43:18 -0700

    working on database

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ const firebaseConfig = {
     messagingSenderId: "1060692712600",
     appId: "1:1060692712600:web:d6ad0c9ea0d24a9f6644ed",
     measurementId: "G-5PELZWX7Y0"
-  };
+};
 
 const app = initializeApp(firebaseConfig);
 const database = getDatabase(app);
