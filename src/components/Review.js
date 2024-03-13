511fac2a5ab728df648384851177c0833f6006c9
kristayamamoto-chin <kristayc@uw.edu>
2024-03-12 20:51:02 -0700

    working on database

--- src/components/Review.js
+++ src/components/Review.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
 function Review(props) {
-
-    let date = new Date(props.info.timestamp);
-    let formattedDate = date.toLocaleTimeString();
-
     return (
-        <div className="review">
-            <div className="date">{formattedDate}</div>
-            <div className="reviewSpace">{props.info.studySpace}</div>
-            <div className="reviewContent">{props.info.content}</div>
-            <div className="reviewComment">{props.info.comment}</div>
-            <div className="likes" onClick={() => props.update(props.id)}>Likes: {props.info.likes}</div>
-        </div>
+
+        <Card className="review">
+            <CardBody>
+                <CardTitle>
+                    <strong>Review on {props.info.studySpaces}</strong>
+                </CardTitle>
+            </CardBody>
+        </Card>
+        // <div className="review">
+        //     <div className="reviewSpace">{props.info.studySpace}</div>
+        //     <div className="reviewContent">{props.info.content}</div>
+        //     <div className="reviewComment">{props.info.comment}</div>
+        //     <div className="likes" onClick={() => props.update(props.id)}>Likes: {props.info.likes}</div>
+        // </div>
     )
 }
 
